3300f7fd51d4dec2d7cee561422f2e3902f400c5
Zlatko Gantchev <zlatko.gantchev@gmail.com>
2017-01-13 17:54:27 -0500

    Finished migrating API functions to api.js

--- api.js
+++ api.js
@@ -1,5 +1,6 @@
 
 var Post = require('./models/post');
+var User = require('./models/user');
 
 // GET request
 exports.getPostByTitle = function(req, res){
@@ -9,4 +10,63 @@ exports.getPostByTitle = function(req, res){
     });
 };
 
+// POST request
+exports.createPost = function(req, res){
+  res.send('Received JSON data!');
 
+  var newPost = {
+    title: req.body.title,
+    address: req.body.address,
+    description: req.body.description
+  };
+
+  //create new database entry from POST request's JSON object
+  Post.create(newPost, function(err, results){
+    if (err) console.log(err);
+    console.log('Successfully uploaded new post');
+
+    //retrieve new db object & log its attributes to console
+    Post.findOne({'title': newPost.title}, 'title address description date', function(err, post) {
+      if (err) return handleError(err);
+
+      console.log(post.title);
+      console.log(post.address);
+      console.log(post.date);
+      console.log(post.description);
+    });
+  });
+};
+
+// PUT requests
+exports.updateUserInfo = function(err, res){
+  res.send('Got a put request at /user');
+  User.findByIdAndUpdate({_id: req.params.id}, {$set: {phone: req.body.phone}}, {new: true}, function(err, User) {
+    if(err) return handleError(err);
+    Console.log('Update user phone number successful');
+  });
+};
+
+exports.updatePostInfo = function (req, res) {
+  res.send('Got a PUT request at /post');
+  Post.findByIdAndUpdate({_id: req.params.id}, {$set: {title: req.body.title}}, {new: true}, function(err, Post) {
+  if (err) return handleError(err);
+  console.log('Update post title successful!');
+  });
+};
+
+// DELETE requests
+exports.deleteUser = function (req, res) {
+  res.send('Got a DELETE request at /user');
+  User.findOneAndRemove({'_id': req.params.id}, function(err, result){
+    if(err) return handleError(err);
+    console.log('Delete user successful!');
+  });
+};
+
+exports.deletePost = function (req, res) {
+  res.send('Got a DELETE request at /post');
+  Post.findOneAndRemove({'_id': req.params.id}, function(err, result){
+    if(err) return handleError(err);
+    console.log('Delete post successful!');
+  });
+};
--- server.js
+++ server.js
@@ -4,8 +4,6 @@ var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var path = require('path');
 var mongoose = require('mongoose');
-//var Post = require('./models/post');
-var User = require('./models/user');
 var api = require('./api');
 
 var app = express();
@@ -21,77 +19,19 @@ app.use('/assets', express.static(path.join(__dirname, 'public')));
 
 mongoose.connect('mongodb://localhost/sellit');
 
-// GET request
+// GET requests
 app.get('/getByTitle', api.getPostByTitle);
 
-// POST request
-app.post('/', function (req, res) {
-  res.send('Got a POST request')
-});
+// POST requests
+app.post('/createPost', api.createPost);
 
-app.post('/createPost', function(req, res){
-  res.send('Received JSON data!');
+// PUT requests
+app.put('/user/:id', api.updateUserInfo);
+app.put('/post/:id', api.updatePostInfo);
 
-  var newPost = {
-    title: req.body.title,
-    address: req.body.address,
-    description: req.body.description
-  };
-
-  //create new database entry from POST request's JSON object
-  Post.create(newPost, function(err, results){
-    if (err) console.log(err);
-    console.log('Successfully uploaded new post');
-
-    //retrieve new db object & log its attributes to console
-    Post.findOne({'title': newPost.title}, 'title address description date', function(err, post) {
-      if (err) return handleError(err);
-
-      console.log(post.title);
-      console.log(post.address);
-      console.log(post.date);
-      console.log(post.description);
-    });
-  });
-
-});
-
-//Put request to /user
-app.put('/user/:id', function(err, res){
-  res.send('Got a put request at /user');
-  User.findByIdAndUpdate({_id: req.params.id}, {$set: {phone: req.body.phone}}, {new: true}, function(err, User) {
-    if(err) return handleError(err);
-    Console.log('Update user phone number successful');
-  });
-});
-
-// PUT request to /post
-app.put('/post/:id', function (req, res) {
-  res.send('Got a PUT request at /post');
-  Post.findByIdAndUpdate({_id: req.params.id}, {$set: {title: req.body.title}}, {new: true}, function(err, Post) {
-  if (err) return handleError(err);
-  console.log('Update post title successful!');
-  //res.send(Post);
-  });
-});
-
-// DELETE request to /user
-app.delete('/user', function (req, res) {
-  res.send('Got a DELETE request at /user');
-  User.findOneAndRemove({'_id': req.params.id}, function(err, result){
-    if(err) return handleError(err);
-    console.log('Delete user successful!');
-  });
-});
-
-// DELETE request to /post
-app.delete('/post/:id', function (req, res) {
-  res.send('Got a DELETE request at /user');
-  Post.findOneAndRemove({'_id': req.params.id}, function(err, result){
-    if(err) return handleError(err);
-    console.log('Delete post successful!');
-  });
-});
+// DELETE requests
+app.delete('/user', api.deleteUser);
+app.delete('/post/:id', api.deletePost);
 
 app.listen(3000, function(){
   console.log('Server running on port: 3000');
