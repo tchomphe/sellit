fbec5c29e3d4f4522b4a016300c9ff5a9b1e565f
admin <admin@admins-MacBook-Pro.local>
2017-02-24 22:04:24 -0500

    Partial Passport implementation. Not working!

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ var path = require('path');
 var mongoose = require('mongoose');
 var api = require('./api');
 
+//var User = require('./models/user');
+var passport = require('passport'),
+ LocalStrategy = require('passport-local').Strategy;
+
 var app = express();
 
 // Logger to print request/response to server
@@ -19,6 +23,28 @@ app.use('/assets', express.static(path.join(__dirname, 'public')));
 
 mongoose.connect('mongodb://localhost/sellit');
 
+passport.use(new LocalStrategy(
+  function(username, password, done){    
+    console.log('About to check db');
+    User.findOne({ username:username }, function(err, user){
+      console.log('Just checked db');
+      if(err) {
+        console.log('Error! ' + err);
+        return done(err); }
+      if(!user) {
+        console.log('User not found');
+        return done(null, false, {message: 'Incorrect username'});
+      }
+      if(!user.validPassword(password)){
+        console.log('Invalid PW');
+        return done(null, false, {message: 'Incorrect password'});
+      }
+      console.log('Username: ' + username + ', password: ' + password);
+      return done(null, user);
+    });
+  }
+));
+
 // GET request
 app.get('/getByTitle', api.getPostByTitle);
 app.get('/getUsername/:id', api.getUsernameByID);
@@ -27,6 +53,10 @@ app.get('/getUsername/:id', api.getUsernameByID);
 app.post('/', function (req, res) {
   res.send('Got a POST request')
 });
+app.post('/login', passport.authenticate('local', function(req, res){
+  console.log('BRAP! Authentication passed nikka! ');
+  //res.redirect('/login/success');
+}));
 
 app.post('/createPost', api.createPost);
 
@@ -41,6 +71,10 @@ app.delete('/user', api.deleteUser);
 
 // DELETE request to /post
 app.delete('/post/:id', api.deletePost);
+
+// Post request to /login
+app.post('/login', passport.authenticate('local', { successRedirect: '/',
+                                                    failureRedirect: '/login' }));
 
 app.listen(3000, function(){
   console.log('Server running on port: 3000');
