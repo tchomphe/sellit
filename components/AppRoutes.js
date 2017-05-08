acc65e2c9d5b3efcf5a242e12306e414638576fc
Zlatko Gantchev <zlatko.gantchev@gmail.com>
2017-05-07 22:14:24 -0400

    Fixed react-router issue, React Components rendering successfully

--- components/AppRoutes.js
+++ components/AppRoutes.js
@@ -1,23 +1,20 @@
 'use strict';
 
 import React from 'react';
-//import { Router, Route, IndexRoute} from 'react-router';
-import { BrowserRouter as Router, Route, IndexRoute, browserHistory } from 'react-router-dom';
+import { BrowserRouter as Router, Route, hashHistory } from 'react-router-dom';
 
 import Layout from './Layout';
 import MainPage from './MainPage';
 // import NotFoundPage from 'NotFoundPage';
 
-const routes = (
-  <Route path="/" component={Layout}>
-    <IndexRoute component={MainPage}/>
-  </Route>
-);
-
 export default class AppRoutes extends React.Component {
   render() {
     return (
-      <Router history={browserHistory} routes={routes} onUpdate={() => window.scrollTo(0, 0)}/>
-    );
-  }
+      <Router history={hashHistory}>
+        <Layout>
+          <Route path="/" component={MainPage}/>
+        </Layout>
+      </Router>
+      );
+    }
 }
