9bc919c94d529d70aceb31cccf8ff0ee4d1eade4
Zlatko Gantchev <zlatko.gantchev@gmail.com>
2017-05-20 16:19:16 -0400

    Knighted Layout as the container-fluid BootStrap class

--- components/Layout.js
+++ components/Layout.js
@@ -3,14 +3,8 @@ import React from 'react';
 class Layout extends React.Component {
     render(){
         return(
-            <div className="app-layout-container">
-                <header>
-                    <h2>HEADER</h2>
-                </header>
+            <div className="app-layout container-fluid">
                 <div className="app-content">{this.props.children}</div>
-                <footer>
-                    <h2>FOOTER</h2>
-                </footer>
             </div>
         );
     }
