02b184bf52ff24a7c871ea43b6c78f5122c284d4
admin <admin@admins-MacBook-Pro.local>
2017-05-24 21:29:49 -0400

    Installed superagent to connect react component with the server

--- components/NavigationWindow.js
+++ components/NavigationWindow.js
@@ -8,17 +8,15 @@ class NavigationWindow extends React.Component{
             <div>
                 {/*<!-- Button trigger modal -->*/}
                 <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#navModal">
-                Launch demo modal
+                Launch Search bar
                 </button>
 
                 {/*<!-- Modal -->*/}
-                <div className="modal fade" id="navModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div className="modal-dialog" role="document">
-                    <div className="modal-content">
-                    
+                <div className="modal fade" id="navModal" tabIndex="-1" role="dialog">
+                <div className="modal-dialog">
+                    <div className="modal-content">                    
                     <div className="modal-header">                        
-                    </div>
-                        
+                    </div>                        
                     <div className="modal-body">            
                         <div class="row">
                             <input id="1" class="form-control" type="text" name="search" placeholder="Search..." required/>
@@ -34,11 +32,9 @@ class NavigationWindow extends React.Component{
                             </span>
                         </div>                        
                     </div>
-
                     <div className="modal-footer">
                         <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-                    </div>
-                    
+                    </div>             
                     </div>
                 </div>
                 </div>
--- components/TilesContainer.js
+++ components/TilesContainer.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import Tile from './Tile';
 import NavigationWindow from './NavigationWindow'; 
+import Request from 'superagent';
 
 export default class TilesContainer extends React.Component {
+    constructor(props){
+        super(props); 
+        this.state = {
+            title:[]
+        };               
+    }
+    componentWillMount(){
+        Request.get('http://localhost:3000/getPostPage/1').then((res) => {
+            this.setState({title: res.body.docs[0].title});
+        });
+    }
     render(){
         return(
             <div className="col-12">
                 <div className="tiles-container card-columns">
+                    {this.state.title}
                     <Tile /><Tile /><Tile />
                     <Tile /><Tile /><Tile />
                     <Tile /><Tile /><Tile />
