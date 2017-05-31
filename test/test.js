1fc83a8eb53a5925a7c3625862d9f1635874c1fb
admin <admin@admins-MacBook-Pro.local>
2017-05-31 17:16:36 -0400

    Fixed mocha test by adding done callback

--- test/test.js
+++ test/test.js
@@ -1,9 +1,10 @@
 var expect = require('chai').expect;
 var request = require('superagent');
 
-it('Initial request status', function() {
-    request('http://localhost:8080' , function(error, response, body) {
-        expect(response.statusCode).to.equal(200);
+it('Initial request status', function(done) {
+    request('http://localhost:3000' , function(error, response, body) {
+            expect(response.statusCode).to.equal(200);
+            done();
     });
 });
 
