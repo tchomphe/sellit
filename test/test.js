35255e3bfcef1bddef746ea0f05a8dee21ea676d
Zlatko Gantchev <zlatko.gantchev@gmail.com>
2017-06-02 16:55:55 -0400

    Removed server start up, and take down script

--- test/test.js
+++ test/test.js
@@ -2,16 +2,6 @@ var expect = require('chai').expect;
 var request = require('superagent');
 var server = require('../server');
 
-describe('Start up server for testing', function () {
-  before(function () {
-    server.listen();
-  });
-
-  after(function () {
-    server.close();
-  });
-});
-
 it('checks request status', function(done) {
     request('http://localhost:8080' , function(error, response, body) {
             expect(response.statusCode).to.equal(200);
