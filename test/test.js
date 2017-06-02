6a68fef9f8729adeca366f0029a61f8d7250938c
Zlatko Gantchev <zlatko.gantchev@gmail.com>
2017-06-02 17:11:02 -0400

    Wrapped api testing in appropriate descriptor

--- test/test.js
+++ test/test.js
@@ -2,9 +2,15 @@ var expect = require('chai').expect;
 var request = require('superagent');
 var server = require('../server');
 
-it('checks request status', function(done) {
-    request('http://localhost:8080' , function(error, response, body) {
-            expect(response.statusCode).to.equal(200);
-            done();
+describe('api tests', function () {
+
+  describe('GET /', function(){
+    it('responds with 200 OK', function(done) {
+      request('http://localhost:8080' , function(error, response, body) {
+        expect(response.statusCode).to.equal(200);
+        done();
+      });
     });
+  });
+
 });
