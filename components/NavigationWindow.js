94b2c16ee10857c5d762bf539381f4796f589a54
Zlatko Gantchev <zlatko.gantchev@gmail.com>
2017-06-13 16:40:01 -0400

    Stylized and added button to Navigation Window

--- components/NavigationWindow.js
+++ components/NavigationWindow.js
@@ -7,26 +7,19 @@ class NavigationWindow extends React.Component{
         return(
             <div>
                 <CornerMenu />
-
-                {/*<!-- Modal Structure -->*/}
                 <div id="navModal" className="modal">
                     <div className="modal-content">
-                    <nav>
-                        <div className="nav-wrapper">
-                        <form>
+                        <nav><form>
                             <div className="input-field">
-                            <input id="search" type="search" required />
-                            <label className="label-icon" for="search"><i className="material-icons">search</i></label>
-                            <i className="material-icons">close</i>
+                                <input id="search" type="search" required />
+                                <label className="label-icon" for="search"><i className="material-icons">search</i></label>
                             </div>
-                        </form>
-                        </div>
-                    </nav>
-                    </div>
-                    <div className="modal-footer">
-                    <a href="#!" className="modal-action modal-close waves-effect waves-green btn-flat">Close</a>
+                        </form></nav>
+                        <br />
+                        <a className="waves-effect waves-light btn-large login-button">login to post</a>
                     </div>
                 </div>
+
             </div>
         );
     }
