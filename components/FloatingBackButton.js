0dbdbd4606b5d3e0d461f61f91c248602b437af6
Zlatko Gantchev <zlatko.gantchev@gmail.com>
2017-06-14 13:04:36 -0400

    Repositioned floating Back button and modified css classes for both floating buttons

--- components/FloatingBackButton.js
+++ components/FloatingBackButton.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class FloatingBackButton extends React.Component {
     render(){
         return(
-            <div>
+            <div className="floatingBackButton">
                 <a className="btn-floating btn-large waves-effect waves-light red">
                     <img src="../assets/icons/back.svg" />
                 </a>
--- components/FloatingMenuButton.js
+++ components/FloatingMenuButton.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class FloatingMenuButton extends React.Component {
     render(){
         return(
-            <div className="corner-menu">
+            <div className="floatingMenuButton">
                 <a className="btn-floating btn-large waves-effect waves-light red" href="#navModal">
                     <i className="material-icons">search</i>
                 </a>
