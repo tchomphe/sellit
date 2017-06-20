c21e115ae7dd40a5dce5b3723fb3b7489721b787
Zlatko Gantchev <zlatko.gantchev@gmail.com>
2017-06-20 15:34:05 -0400

    Re-organized tests into three sections

--- test/test.js
+++ test/test.js
@@ -6,7 +6,7 @@ var server = require('../server');
 // temporary variables for storing ID's
 var postID, userID;
 
-describe('API tests', function () {
+describe('User-related API tests', function () {
 
   describe('GET /', function(){
     it('responds with HTTP Status 200', function(done) {
@@ -19,24 +19,6 @@ describe('API tests', function () {
 
 // Create //
 // -------//
-
-  describe('POST /createPost', function(){
-    it('responds with HTTP Status 200', function(done) {
-      request
-        .post('http://localhost:8080/createPost')
-        .set('Content-Type', 'multipart/form-data')
-        .field('title', 'iPhone 34s')
-        .field('address', 'A1B2C3')
-        .field('type', 'Phone')
-        .field('description', 'Test description here!')
-        .attach('postImages', __dirname + '/image1.jpg')
-        .attach('postImages', __dirname + '/image2.jpg')
-        .end(function(error, response, body){
-          expect(response.statusCode).to.equal(200);
-          done();
-        });
-    })
-  });
 
   describe('POST /createUser', function(){
     it('responds with HTTP Status 200', function(done) {
@@ -55,27 +37,6 @@ describe('API tests', function () {
 
 // Retrieve //
 // -------- //
-
-  describe('GET /postByTitle/:title', function(){
-    it('responds with HTTP Status 200', function(done) {
-      request('http://localhost:8080/postByTitle/iPhone%2034s', function(error, response, body){
-        expect(response.statusCode).to.equal(200);
-        console.log(response.body);
-        postID = response.body._id;
-        done();
-      });
-    });
-  });
-
-  describe('GET /post/:id', function(){
-    it('responds with HTTP Status 200', function(done) {
-      request.get('http://localhost:8080/post/'+postID, function(error, response, body){
-        expect(response.statusCode).to.equal(200);
-        console.log(response.body);
-        done();
-      })
-    })
-  });
 
   describe('GET /userByEmail/:email', function(){
     it('responds with HTTP Status 200', function(done) {
@@ -101,19 +62,6 @@ describe('API tests', function () {
 // Update //
 // -------//
 
-  describe('PUT /post/:id', function(){
-    it('responds with HTTP Status 200', function(done){
-      request
-        .put('http://localhost:8080/post/'+postID)
-        .set('Content-Type', 'application/json')
-        .send('{"title":"New Title", "address":"New Address", "description":"New Description"}')
-        .end(function(error, response, body){
-          expect(response.statusCode).to.equal(200);
-          done();
-        });
-    });
-  });
-
   describe('PUT /user/:id', function(){
     it('responds with HTTP Status 200', function(done){
       request
@@ -126,9 +74,66 @@ describe('API tests', function () {
         });
     });
   });
+});
 
-// Delete //
-// -------//
+
+describe('Post-related API tests', function () {
+
+  describe('POST /createPost', function(){
+    it('responds with HTTP Status 200', function(done) {
+      request
+        .post('http://localhost:8080/createPost')
+        .set('Content-Type', 'multipart/form-data')
+        .field('title', 'iPhone 34s')
+        .field('address', 'A1B2C3')
+        .field('type', 'Phone')
+        .field('description', 'Test description here!')
+        .attach('postImages', __dirname + '/image1.jpg')
+        .attach('postImages', __dirname + '/image2.jpg')
+        .end(function(error, response, body){
+          expect(response.statusCode).to.equal(200);
+          done();
+        });
+    })
+  });
+
+  describe('GET /postByTitle/:title', function(){
+    it('responds with HTTP Status 200', function(done) {
+      request('http://localhost:8080/postByTitle/iPhone%2034s', function(error, response, body){
+        expect(response.statusCode).to.equal(200);
+        console.log(response.body);
+        postID = response.body._id;
+        done();
+      });
+    });
+  });
+
+  describe('GET /post/:id', function(){
+    it('responds with HTTP Status 200', function(done) {
+      request.get('http://localhost:8080/post/'+postID, function(error, response, body){
+        expect(response.statusCode).to.equal(200);
+        console.log(response.body);
+        done();
+      })
+    })
+  });
+
+  describe('PUT /post/:id', function(){
+    it('responds with HTTP Status 200', function(done){
+      request
+        .put('http://localhost:8080/post/'+postID)
+        .set('Content-Type', 'application/json')
+        .send('{"title":"New Title", "address":"New Address", "description":"New Description"}')
+        .end(function(error, response, body){
+          expect(response.statusCode).to.equal(200);
+          done();
+        });
+    });
+  });
+});
+
+
+describe('Cleanup, and DELETE API tests', function () {
 
   describe('DELETE /post/:id', function(){
     it('responds with HTTP Status 200', function(done){
