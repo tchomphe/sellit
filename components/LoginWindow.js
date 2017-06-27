b2b6043b686a950255bb3fcb924beb6c1ea17009
Zlatko Gantchev <zlatko.gantchev@gmail.com>
2017-06-27 18:04:23 -0400

    Bit of cleanup for the Login component, also replaced anchor link with submit button

--- components/LoginWindow.js
+++ components/LoginWindow.js
@@ -7,24 +7,20 @@ class LoginWindow extends React.Component{
                 <div id="loginModal" className="modal">
                     <a className="modal-action modal-close btn-large modalButtonClose">x</a>
                     <div className="modal-content">
-                         <div className="row">
-                            <form className="col s12 center" method="post" action="/login">
-                                <h5>Login</h5>
-                                <div className="row center">
-                                    <div className="input-field col s12">
-                                        <input id="email" type="email" className="validate" />
-                                        <label htmlFor="email" data-error="wrong" data-success="right">Email</label>
-                                    </div>
-                                </div>
-                                <div className="row">
-                                    <div className="input-field col l6">
-                                        <input id="password" type="password" className="validate" />
-                                        <label htmlFor="password">Password</label>
-                                    </div>
-                                </div>
-                                <a className="waves-effect waves-light btn-large" type="submit">Login</a>
-                            </form>
-                        </div>
+                        <h5>Login</h5>
+                        <form className="row center" method="post" action="/login">
+                            <div className="input-field col s12">
+                                <input id="email" type="email" name="username" className="validate" />
+                                <label htmlFor="email" data-error="wrong" data-success="right">Email</label>
+                            </div>
+                            <div className="input-field col l12">
+                                <input id="password" type="password" name="password" className="validate" />
+                                <label htmlFor="password">Password</label>
+                            </div>
+                            <button className="btn-large waves-effect waves-light" type="submit" name="action">Login
+                                <i className="material-icons right">send</i>
+                            </button>
+                        </form>
                     </div>
                 </div>
             </div>
