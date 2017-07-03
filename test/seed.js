7766c28d600ca8cbc7f03409de9eab2f72c34748
Zlatko Gantchev <zlatko.gantchev@gmail.com>
2017-07-03 18:30:43 -0400

    Created file for mongodb data seeding

---
+++ test/seed.js
@@ -0,0 +1,40 @@
+var User = require('../config/models/user');
+var Post = require('../config/models/post');
+var ObjectID = require('mongodb').ObjectID;
+var vagetaID = ObjectID();
+
+var seedUsers = [
+  {
+    _id: vagetaID,
+    email: 'vageta@gmail.com',
+    password: 'Over 9000!',
+    username: 'Sayan Prince'
+  }
+];
+var seedPosts = [
+  {
+    ownerID: vagetaID,
+    title: 'iCapsure Ultimate',
+    type: 'smartphone',
+    address: '24 Vogo Corp. St'
+  },
+  {
+    ownerID: vagetaID,
+    title: 'BananaPhone',
+    type: 'smartphone',
+    address: '24 Vogo Corp. St',
+  }
+];
+
+module.exports.upload = function() {
+    console.log('Uploading seed data...');
+
+    User.insertMany(seedUsers, function(err, docs){
+        if (err) { console.log(err); }
+    });
+    Post.insertMany(seedPosts, function(err, docs){
+        if (err) { console.log(err); }
+    });
+
+    console.log('Finished uploading seed data.');
+}
