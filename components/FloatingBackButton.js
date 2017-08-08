67a4094fbfdaf44dcde9cbe9bb6963ddcf302e20
Zlatko Gantchev <zlatko.gantchev@gmail.com>
2017-08-07 21:36:45 -0400

    Updated floating back button and floating buttons styling

--- components/FloatingBackButton.js
+++ components/FloatingBackButton.js
@@ -4,8 +4,8 @@ export default class FloatingBackButton extends React.Component {
     render(){
         return(
             <div className="floatingBackButton">
-                <a className="modal-action modal-close btn-floating btn-large waves-effect waves-light red">
-                    <img src="../assets/icons/back.svg" />
+                <a className="modal-action modal-close btn-floating btn-large waves-effect waves-light">
+                    <i className="large material-icons">arrow_back</i>
                 </a>
             </div>
         );
