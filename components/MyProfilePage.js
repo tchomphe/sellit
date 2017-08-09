99cde373553bbd9c12e8fb08a0f3608f6f973286
admin <admin@admins-MacBook-Pro.local>
2017-08-09 16:46:21 -0400

    Modified my profile page and it's css.

--- components/MyProfilePage.js
+++ components/MyProfilePage.js
@@ -15,27 +15,60 @@ export default class MyProfilePage extends React.Component{
     }
 
     render(){
-        return(
+        return(            
             <div className="container">                                                    
                 <div className="row">                    
-                        <h5><center><b>My Profile</b> - update your information</center></h5>                    
-                    <div className="col s6">                         
-                        <h6><b>Account Details</b></h6>
-                        <label for="nickname">Nickname (optional):</label>
-                        <input placeholder={this.state.nickname} id="nickname" type="text" class="validate" />
+                        <h5><center><b>My Profile</b> - update your information</center></h5>                                        
+                        <div className="col s6">                         
+                            <div className="card-panel">
+                                <h6><b>Account Details</b></h6>
+                                <label for="nickname">Nickname (optional):</label>
+                                <input placeholder={this.state.nickname} id="nickname" type="text" class="validate" />
 
-                        <label for="email">Email: </label>
-                        <input placeholder={this.state.email} id="nickname" type="text" class="validate" />
+                                <label for="email">Email: </label>
+                                <input placeholder={this.state.email} id="nickname" type="text" class="validate" />
 
-                        <label for="nickname">Phone (optional):</label>
-                        <input placeholder={this.state.phone} id="nickname" type="text" class="validate" />
+                                <label for="phone">Phone Number (optional):</label>
+                                <input placeholder={this.state.phone} id="phone" type="text" class="validate" />
 
-                        
-                    </div>
-                    <div className="col s6"> 
-                        <h6><b>Account Summary</b></h6>
-                              <p class="z-depth-5">z-depth-5</p>
-                    </div>
+                                <label for="newpassword">New Password:</label>
+                                <input placeholder="New Password" id="newpassword" type="password" class="validate" />
+                                <label for="confirmnewpassword">Confirm New Password:</label>
+                                <input placeholder="Confirm New Password" id="confirmnewpassword" type="password" class="validate" />
+
+                                <label for="password">*Password:</label>
+                                <input placeholder="Password" id="password" type="password" class="validate" />
+
+                                <button class="btn waves-effect waves-light" type="submit" name="action">
+                                    Save Changes
+                                </button>
+                            </div>
+                        </div>
+                        <div className="col s6"> 
+                            <div className="card-panel">
+                                <h6><b>Account Summary</b></h6>
+                                <table>
+                                    <tr>
+                                        Email: 
+                                    </tr>      
+                                    <tr>
+                                        <i>{this.state.email}</i>
+                                    </tr>
+                                    <tr>
+                                        Nickname: 
+                                    </tr>      
+                                    <tr>
+                                        <i>{this.state.nickname}</i>
+                                    </tr>
+                                    <tr>
+                                        Phone Number: 
+                                    </tr>      
+                                    <tr>
+                                        <i>{this.state.phone}</i>
+                                    </tr>                            
+                                </table>
+                            </div>
+                        </div>
                 </div>
             </div>
         )
