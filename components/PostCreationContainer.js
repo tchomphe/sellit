618d29c1216df28813ebfb4ef3b38bf0f8600a83
admin <admin@admins-MacBook-Pro.local>
2017-08-17 01:26:18 -0400

    Modified post create page to function properly using materialize form.

--- components/PostCreationContainer.js
+++ components/PostCreationContainer.js
@@ -4,68 +4,57 @@ import Request from 'superagent';
 class PostCreationContainer extends React.Component{
     render(){
         return(
-            // <div className="container">
-            //     <h5 className="profilePageHeader">Create Post</h5>
-            //     <form className="col s12 center" method="post" action="/createPost" enctype="multipart/form-data">
-            //         <div className="row">
-            //             <div className="input-field col s12 m6">
-            //                 <input id="title" name="title" type="text" className="validate" />
-            //                 <label htmlFor="title" data-error="wrong" data-success="right">* Title</label>
-            //             </div>
-
-            //             <div className="input-field col s12 m6">
-            //                 <select name="type">
-            //                     <option value="Phone" defaultValue>Phone</option>
-            //                     <option value="Laptop">Laptop</option>
-            //                     <option value="Case">Case</option>
-            //                     <option value="Other">Other</option>
-            //                 </select>
-            //                 <label>* Type</label>
-            //             </div>
-
-            //             <div className="input-field col s12 m6">
-            //                 <input id="price" name="price" type="text" className="validate" />
-            //                 <label htmlFor="price" data-error="wrong" data-success="right">Price</label>
-            //             </div>
-
-            //             <div className="input-field col s12 m6">
-            //                 <input id="address" name="address" type="text" className="validate" />
-            //                 <label htmlFor="address" data-error="wrong" data-success="right">* Address</label>
-            //             </div>
-
-            //             <div className="input-field col s12">
-            //                 <textarea id="description" name="description" className="materialize-textarea"></textarea>
-            //                 <label htmlFor="description">Description</label>
-            //             </div>
- 
-            //             <div>
-            //             <div className="file-field input-field">
-            //                 <div className="btn">
-            //                     <span>Browse</span>
-            //                     <input type="file" name="postImages" />
-            //                 </div>
-            //                 <div className="file-path-wrapper">
-            //                     <input type="text" className="file-path validate" placeholder="Upload files" />
-            //                 </div>
-            //             </div>                            
-            //             </div>
-            //             <button className="btn-large waves-effect waves-light" type="submit" name="action">Create Post
-            //                 <i className="material-icons right">send</i>
-            //             </button>
-            //         </div>
-            //     </form>
-            // </div>
-
             <div className="contatiner">
-                <h1>create-post</h1>
-                <form method="post" action="/createPost" encType="multipart/form-data">                    
-                    <input type="text" name="title" />
-                    <input type="text" name="address" />
-                    <input type="text" name="type" />
-                    <input type="file" name="postImages" />
-                    <input type="submit" value="Submit" />
-
-                </form>
+                <h6 className="profilePageHeader">Create-Post</h6>                            
+                <form method="post" action="/createPost" encType="multipart/form-data">
+                <div className="row">
+                    <div className="col s6">
+                        <label>Title</label>
+                        <input type="text" name="title" />
+                    </div>
+                    <div className="col s6">
+                        <label>Address</label>
+                        <input type="text" name="address" />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col s6">
+                        <label>* Type</label>
+                        <select name="type">
+                            <option value="Phone" defaultValue>Phone</option>
+                            <option value="Laptop">Laptop</option>
+                            <option value="Case">Case</option>
+                            <option value="Other">Other</option>
+                        </select>                        
+                    </div>
+                    <div className="col s6">
+                        <label>Price</label>
+                        <input type="text" name="price" />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col s12">
+                        <label>Description</label>
+                        <input type="text" name="description" />
+                    </div>
+                </div>
+                <div className="row">                    
+                    <div className="col s12 file-field input-field">
+                        <div className="btn">
+                            <span>Browse</span>
+                            <input type="file" name="postImages" multiple/>
+                        </div>
+                        <div className="file-path-wrapper">
+                            <input className="file-path validate" type="text" placeholder="Upload multiple files" />
+                        </div>
+                    </div>                        
+                </div>            
+                <div className="row center">
+                    <div className="s12">                
+                        <button className="btn waves-effect waves-light" type="submit" name="action">Create</button>
+                    </div>
+                </div>            
+                </form>                
             </div>
         )
     }
