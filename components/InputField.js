cd48766bdde180c27ca6974482ebd13e2d3242cf
Zlatko Gantchev <zlatko.gantchev@gmail.com>
2017-08-19 10:37:32 -0400

    Added attributes to <label> and moved it below the input, important for success/error messages

--- components/InputField.js
+++ components/InputField.js
@@ -4,20 +4,28 @@ import PropTypes from 'prop-types';
 export default class InputField extends React.Component{
     render(){
         var myFieldClass = (this.props.fieldClass == undefined) ? " col s12" : (" " + this.props.fieldClass),
-            myLabelClass = (this.props.placeholder == undefined) ? "" : "active",
-            myPlaceholder = (this.props.placeholder == undefined) ? "" : this.props.placeholder,
-            myType = (this.props.type == undefined) ? "text" : this.props.type;
+            myLabelClass = (this.props.placeholder == undefined) ? "" : "active", //prevent placeholder & label overlap
+            myLabelSuccess = (this.props.labelSuccess == undefined) ? "" : this.props.labelSuccess,
+            myLabelError = (this.props.labelError == undefined) ? "" : this.props.labelError,
+            myInputType = (this.props.type == undefined) ? "text" : this.props.type,
+            myInputPlaceholder = (this.props.placeholder == undefined) ? "" : this.props.placeholder,
+            myInputRequired = (this.props.required == undefined) ? "" : this.props.required;
 
         return(
             <div className={"input-field" + myFieldClass}>
-                <label htmlFor={this.props.id} className={myLabelClass}>{this.props.labelText}</label>
                 <input
                     id={this.props.id}
                     name={this.props.id}
-                    type={myType}
-                    placeholder={myPlaceholder}
-                    className="validate"
-                    onChange={this.props.onChange} />
+                    type={myInputType}
+                    placeholder={myInputPlaceholder}
+                    onChange={this.props.onChange}
+                    required={myInputRequired}
+                    className="validate" />
+                <label
+                    htmlFor={this.props.id}
+                    data-success={myLabelSuccess}
+                    data-error={myLabelError}
+                    className={myLabelClass}>{this.props.labelText}</label>
             </div>
         )
     }
