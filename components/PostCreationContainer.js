cfe9ebf9846f85867ed80e1f626823f8dbcc13f7
Zlatko Gantchev <zlatko.gantchev@gmail.com>
2017-08-19 21:00:36 -0400

    Integrated InputField, initiated state, and created handle functions

--- components/PostCreationContainer.js
+++ components/PostCreationContainer.js
@@ -1,48 +1,50 @@
 import React from 'react';
+import InputField from './InputField';
 import Request from 'superagent';
 
 class PostCreationContainer extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            err:"",
+            title:"",
+            address:"",
+            price:"",
+            description:""
+        }
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
     componentDidMount(){
         // Initialize the Materialize select
         $('select').material_select();
+    }
+    handleInputChange(event){
+        this.setState({ [event.target.name]: event.target.value });
+    }
+    handleSubmit(event){
+        event.preventDefault();
+
     }
     render(){
         return(
             <div className="contatiner">
                 <h6 className="profilePageHeader">Create-Post</h6>
-                <form method="post" action="/createPost" encType="multipart/form-data">
+                <form onSubmit={this.handleSubmit} >
                 <div className="row">
-                    <div className="col s6">
-                        <label>Title</label>
-                        <input type="text" name="title" />
-                    </div>
-                    <div className="col s6">
-                        <label>Address</label>
-                        <input type="text" name="address" />
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col s6">
-                        <label>* Type</label>
+                    <InputField fieldClass="col s6" labelText="Title" id="title" />
+                    <div className="input-field col s6">
                         <select name="type">
                             <option value="Phone" defaultValue>Phone</option>
                             <option value="Laptop">Laptop</option>
                             <option value="Case">Case</option>
                             <option value="Other">Other</option>
                         </select>
+                        <label>* Type</label>
                     </div>
-                    <div className="col s6">
-                        <label>Price</label>
-                        <input type="text" name="price" />
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col s12">
-                        <label>Description</label>
-                        <input type="text" name="description" />
-                    </div>
-                </div>
-                <div className="row">
+                    <InputField fieldClass="col s6" labelText="Address" id="address" />
+                    <InputField fieldClass="col s6" labelText="Price" id="price" />
+                    <InputField labelText="Description" id="description" />
                     <div className="col s12 file-field input-field">
                         <div className="btn">
                             <span>Browse</span>
