4635d8af685b46044c98dcf7dc9f3621bfa4e875
Zlatko Gantchev <zlatko.gantchev@gmail.com>
2017-08-19 21:44:56 -0400

    Created working superagent request with all form fields except file uploads

--- components/PostCreationContainer.js
+++ components/PostCreationContainer.js
@@ -6,11 +6,11 @@ class PostCreationContainer extends React.Component{
     constructor(props){
         super(props);
         this.state={
-            err:"",
-            title:"",
-            address:"",
-            price:"",
-            description:""
+            err: "",
+            title: "",
+            address: "",
+            price: "",
+            description: "",
         }
         this.handleInputChange = this.handleInputChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -25,16 +25,37 @@ class PostCreationContainer extends React.Component{
     handleSubmit(event){
         event.preventDefault();
 
+        //Materialize select can't handle onChange.. so we have to grab value directly
+        var selectType = $('select').val();
+        Request
+            .post('/createPost')
+            .type('form')
+            .send({
+                title: this.state.title,
+                type: selectType,
+                address: this.state.address,
+                price: this.state.price,
+                description: this.state.description,}) //TODO: add file attachments
+            .end((err, res) => {
+                if(err){
+                    this.setState({err: res.body.error});
+                } else {
+                    this.setState({err: ""});
+                    Materialize.toast('Post has been created!', 4000)
+                }
+            });
     }
     render(){
         return(
             <div className="contatiner">
                 <h6 className="profilePageHeader">Create-Post</h6>
+                <div className="card-content red-text">{this.state.err}</div>
                 <form onSubmit={this.handleSubmit} >
                 <div className="row">
-                    <InputField fieldClass="col s6" labelText="Title" id="title" />
+                    <InputField fieldClass="col s6" labelText="* Title"
+                        id="title" onChange={this.handleInputChange} required="required" />
                     <div className="input-field col s6">
-                        <select name="type">
+                        <select id="type" name="type" required="required" >
                             <option value="Phone" defaultValue>Phone</option>
                             <option value="Laptop">Laptop</option>
                             <option value="Case">Case</option>
@@ -42,9 +63,10 @@ class PostCreationContainer extends React.Component{
                         </select>
                         <label>* Type</label>
                     </div>
-                    <InputField fieldClass="col s6" labelText="Address" id="address" />
-                    <InputField fieldClass="col s6" labelText="Price" id="price" />
-                    <InputField labelText="Description" id="description" />
+                    <InputField fieldClass="col s6" labelText="* Address"
+                        id="address" onChange={this.handleInputChange} required="required" />
+                    <InputField fieldClass="col s6" labelText="Price" id="price" onChange={this.handleInputChange} />
+                    <InputField labelText="Description" id="description" onChange={this.handleInputChange} />
                     <div className="col s12 file-field input-field">
                         <div className="btn">
                             <span>Browse</span>
