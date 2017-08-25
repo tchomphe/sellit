55c83114ebf190703d938f9971773b28450f70d6
Zlatko Gantchev <zlatko.gantchev@gmail.com>
2017-08-25 16:57:44 -0400

    Updated myInputPlaceholder with this.props.value; value attribute will only display if a truthy prop is passed

--- components/InputField.js
+++ components/InputField.js
@@ -4,11 +4,10 @@ import PropTypes from 'prop-types';
 export default class InputField extends React.Component{
     render(){
         var myFieldClass = (this.props.fieldClass == undefined) ? " col s12" : (" " + this.props.fieldClass),
-            myLabelClass = (this.props.placeholder == undefined) ? "" : "active", //prevent placeholder & label overlap
+            myLabelClass = (this.props.value == undefined) ? "" : "active", //prevent placeholder & label overlap
             myLabelSuccess = (this.props.labelSuccess == undefined) ? "" : this.props.labelSuccess,
             myLabelError = (this.props.labelError == undefined) ? "" : this.props.labelError,
             myInputType = (this.props.type == undefined) ? "text" : this.props.type,
-            myInputPlaceholder = (this.props.placeholder == undefined) ? "" : this.props.placeholder,
             myInputRequired = (this.props.required == undefined) ? "" : this.props.required;
 
         return(
@@ -17,9 +16,9 @@ export default class InputField extends React.Component{
                     id={this.props.id}
                     name={this.props.id}
                     type={myInputType}
-                    placeholder={myInputPlaceholder}
                     onChange={this.props.onChange}
                     required={myInputRequired}
+                    value={this.props.value /* will only be included on truthy value */ }
                     className="validate" />
                 <label
                     htmlFor={this.props.id}
