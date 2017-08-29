06ad361f70de7c0d11c50631a6387cee090f64a7
admin <admin@admins-MacBook-Pro.local>
2017-08-28 20:13:03 -0400

    Maps working. Replaced spaces with + in address for google api request purpose.

--- components/Map.js
+++ components/Map.js
@@ -5,15 +5,21 @@ class Map extends Component {
   constructor(props){
     super(props);
     this.state = {
-      address: "96 Jameson Ave., Toronto, ON, Canada",
-      zoom: 12
+      address: this.props.address,
+      zoom: 13
     }
   }  
 
   render() {
+    var filtered_address = (this.props.address).replace(/\s/g, "+");
+    var glink = "https://maps.googleapis.com/maps/api/staticmap?center="+filtered_address.toString()+"&zoom=13&size=640x400&maptype=roadmapkey=AIzaSyBcxF_7FH1aEC4g6CGCvq7WPz1LCisZt3A";
     return (
       <div>
-      <img src="https://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=13&size=640x400&maptype=roadmapkey=AIzaSyBcxF_7FH1aEC4g6CGCvq7WPz1LCisZt3A" />
+         {/* <img src="https://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=13&size=640x400&maptype=roadmapkey=AIzaSyBcxF_7FH1aEC4g6CGCvq7WPz1LCisZt3A" />    */}
+            <img src={glink} />    
+        <p>this.props.address: {this.props.address}</p>
+        <p>this.state.address: {this.state.address}</p>
+        <p>filtered_address: {filtered_address}</p>
       </div>
     );
   }
