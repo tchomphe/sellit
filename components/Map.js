c2c6190c1dc6139fd9bb1da9b6b997f89069836b
admin <admin@admins-MacBook-Pro.local>
2017-08-30 14:34:43 -0400

    Moved geocode request to render section.

--- components/Map.js
+++ components/Map.js
@@ -11,14 +11,29 @@ class Map extends Component {
       err: "",      
     }
   }  
-  componentWillMount(){    
+  // componentWillMount(){    
+  //   var filtered_address = (this.props.address).replace(/\s/g, "+");  
+  //   Request
+  //     .get("https://maps.googleapis.com/maps/api/geocode/json?address="+filtered_address.toString()+"&key=AIzaSyD86X7QJpRthF_OfvzcftUNlPg1kzg6sKo")
+  //     .get('https://maps.googleapis.com/maps/api/geocode/json?address=96+Jameson+Ave,+Toronto+ON&key=AIzaSyD86X7QJpRthF_OfvzcftUNlPg1kzg6sKo')
+  //     .end((err, res) => {
+  //       if(err){
+  //         // console.log('error!')
+  //       }
+  //       else{          
+  //         this.setState({
+  //           lat: res.body.results[0].geometry.location.lat,
+  //           lng: res.body.results[0].geometry.location.lng,
+  //         });          
+  //       }
+  //     });
+  //   }
+
+  render() {
     var filtered_address = (this.props.address).replace(/\s/g, "+");
-    // console.log('componentWillMount:this.props.address:'+(this.props.address).toString());
-    // console.log('componentWillMount:this.state.address:'+(this.state.address).toString());
-    // console.log('FILTERED ADDRESS:'+ filtered_address);
     Request
-      // .get("https://maps.googleapis.com/maps/api/geocode/json?address="+filtered_address.toString()+"&key=AIzaSyD86X7QJpRthF_OfvzcftUNlPg1kzg6sKo")
-      .get('https://maps.googleapis.com/maps/api/geocode/json?address=96+Jameson+Ave,+Toronto+ON&key=AIzaSyD86X7QJpRthF_OfvzcftUNlPg1kzg6sKo')
+      .get("https://maps.googleapis.com/maps/api/geocode/json?address="+filtered_address.toString()+"&key=AIzaSyD86X7QJpRthF_OfvzcftUNlPg1kzg6sKo")
+      // .get('https://maps.googleapis.com/maps/api/geocode/json?address=96+Jameson+Ave,+Toronto+ON&key=AIzaSyD86X7QJpRthF_OfvzcftUNlPg1kzg6sKo')
       .end((err, res) => {
         if(err){
           // console.log('error!')
@@ -30,10 +45,6 @@ class Map extends Component {
           });          
         }
       });
-    }
-
-  render() {
-    // var filtered_address = (this.props.address).replace(/\s/g, "+");
     var glink = "https://maps.googleapis.com/maps/api/staticmap?center="+this.state.lat+","+this.state.lng+"&zoom=15&size=400x400&maptype=roadmap&markers=color:red%7Clabel:C%7C"+this.state.lat+','+this.state.lng+"&key=AIzaSyBcxF_7FH1aEC4g6CGCvq7WPz1LCisZt3A";
     return (
       <div>         
@@ -41,7 +52,7 @@ class Map extends Component {
         <img src={glink} />    
         <p>this.props.address: {this.props.address}</p>
         <p>this.state.address: {this.state.address}</p>
-        {/* <p>filtered_address: {filtered_address}</p>   */}
+         <p>filtered_address: {filtered_address}</p>   
         <p>lat: {this.state.lat}, lng: {this.state.lng}</p>                
       </div>
     );
