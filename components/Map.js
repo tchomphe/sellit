132c34955d4377176b3ed20f8578f33f531002d6
admin <admin@admins-MacBook-Pro.local>
2017-09-08 16:12:07 -0400

    Reverted componentWillReceiveProps to componentDidMount since postModal does not receive empty props anymore.

--- components/Map.js
+++ components/Map.js
@@ -14,8 +14,8 @@ class Map extends Component {
 
     // Geocoding done in componentWillReceiveProps because props are empty in componentDidMount on it's first run. 
     // First run is empty because MainPage calls PostModal with empty props in it's initial state. 
-    componentWillReceiveProps(nextProps){
-    var filtered_address = (nextProps.address).replace(/\s/g, "+");
+    componentDidMount(){
+    var filtered_address = (this.props.address).replace(/\s/g, "+");
     Request
       .get("https://maps.googleapis.com/maps/api/geocode/json?address="+filtered_address.toString()+"&key=AIzaSyD86X7QJpRthF_OfvzcftUNlPg1kzg6sKo")
       // .get('https://maps.googleapis.com/maps/api/geocode/json?address=96+Jameson+Ave,+Toronto+ON&key=AIzaSyD86X7QJpRthF_OfvzcftUNlPg1kzg6sKo')
