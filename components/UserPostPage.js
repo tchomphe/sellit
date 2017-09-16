f8e9668fe41575d0312b09d699f8a5fd8500fb3d
admin <admin@admins-MacBook-Pro.local>
2017-09-15 20:31:07 -0400

    Created requestUserPosts method with hard-coded request.

--- components/UserPostPage.js
+++ components/UserPostPage.js
@@ -1,6 +1,32 @@
 import React from 'react';
+import Request from 'superagent';
 
 class UserPostPage extends React.Component{    
+    constructor(props){
+        super(props);
+        this.state = {
+            email: this.props.email,
+            postTiles: null
+        }
+    }
+
+    requestUserPosts(){
+        // Request.get('/searchByOwnerEmail/' + this.state.email).then((res) => {
+        Request.get('/searchByOwnerEmail/tashi.chomphel@gmail.com').then((res) => {
+            var userPosts = req.body.map((post,index) => 
+                <PostTile
+                    nextPostId={(index+1)} 
+                    prevPostId={(index-1)}
+                    key={index}                                        
+                    postModalID={'postModal'+index}
+                    post={post} />    
+                );
+
+                this.setState({
+                    postTiles: userPosts
+                });
+        });
+    }
 
     render(){
         console.log('UserPostPage rendering ...');
