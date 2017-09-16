bdd4415bb08dfd9e53de92f239cc1db20baacc49
admin <admin@admins-MacBook-Pro.local>
2017-09-15 21:28:47 -0400

    Displaying user posts from the hard-coded request.

--- components/UserPostPage.js
+++ components/UserPostPage.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import Request from 'superagent';
+import PostModal from './PostModal';
+import PostTile from './PostTile';
+import { Link, withRouter } from 'react-router-dom';
 
 class UserPostPage extends React.Component{    
     constructor(props){
         super(props);
         this.state = {
-            email: this.props.email,
+            email: null,
             postTiles: null
         }
     }
+    componentDidMount(){        
+        // Initiate Materialize Modal
+        $('.modal').modal();
+
+        // Populate PostTile's with current user's posts
+        this.requestUserPosts();
+    }
 
     requestUserPosts(){
-        // Request.get('/searchByOwnerEmail/' + this.state.email).then((res) => {
+        // Request.get('/searchByOwnerEmail/' + this.props.email).then((res) => {
         Request.get('/searchByOwnerEmail/tashi.chomphel@gmail.com').then((res) => {
-            var userPosts = req.body.map((post,index) => 
+            var userPosts = res.body.map((post,index) => 
                 <PostTile
                     nextPostId={(index+1)} 
                     prevPostId={(index-1)}
@@ -32,8 +42,9 @@ class UserPostPage extends React.Component{
         console.log('UserPostPage rendering ...');
         return(
             <div className="app-content row center">
-                <h4 className="profilePageHeader">User's Posts</h4>
-                BODY                
+                <h4 className="profilePageHeader">All User's Posts</h4>
+                {this.state.postTiles}
+                {/* <p>this.props.params.email: {this.props.match.params.email}</p> */}                
             </div>
         )
     }
