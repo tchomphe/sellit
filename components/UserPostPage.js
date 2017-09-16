63d873a12e51a4f14168dfd015b2ee570b54b1a4
admin <admin@admins-MacBook-Pro.local>
2017-09-15 20:22:13 -0400

    Created User's post page component to view all post from the user.

---
+++ components/UserPostPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class UserPostPage extends React.Component{    
+
+    render(){
+        console.log('UserPostPage rendering ...');
+        return(
+            <div className="app-content row center">
+                <h4 className="profilePageHeader">User's Posts</h4>
+                BODY                
+            </div>
+        )
+    }
+}
+
+export default UserPostPage;
