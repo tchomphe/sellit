1769db95a674726eefae37b2d603ac655a48ff17
admin <tashi.chomphel@gmail.com>
2017-10-06 00:43:09 -0400

    Fixed test. Changed api post/:type to posts/:type.

--- test/test.js
+++ test/test.js
@@ -14,15 +14,15 @@ seeder.upload();
 // Session-less API Tests ============================================ //
 describe('Session-less API tests,', function () {
   
-  describe('GET /post/:type', function(){
+  describe('GET /posts/:type', function(){
     it('responds with HTTP status 200', function(done){
-      request('http://localhost:8080/post/' + seeder.posts[0].type, function(error, response, body){
+      request('http://localhost:8080/posts/' + seeder.posts[0].type, function(error, response, body){
         expect(response.statusCode).to.equal(200);
         done();
       });
     });
   });
-
+  
   describe('GET /', function(){
     it('responds with HTTP Status 200', function(done) {
       request('http://localhost:8080' , function(error, response, body) {
