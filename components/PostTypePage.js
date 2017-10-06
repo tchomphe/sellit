6ba7f4d64bced5a453855246e5c7c73dd7633deb
admin <tashi.chomphel@gmail.com>
2017-10-06 12:27:29 -0400

    Created PostTypePage component to render posts by type. Currently linked to Phone link.

---
+++ components/PostTypePage.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import Request from 'superagent';
+import PostModal from './PostModal';
+import PostTile from './PostTile';
+import { Link, withRouter } from 'react-router-dom';
+
+class PostTypePage extends React.Component{    
+    constructor(props){
+        super(props);
+        this.state = {            
+            postTiles: null
+        }
+    }
+    componentDidMount(){        
+        // Initiate Materialize Modal
+        $('.modal').modal();        
+        
+        // Populate PostTile's with current user's posts
+        this.requestUserPosts();        
+    }
+
+    requestUserPosts(){
+        Request.get('/posts/' + this.props.match.params.type).then((res) => {        
+        // Request.get('/posts/Phone').then((res) => {        
+            var userPosts = res.body.map((post,index) => 
+                <PostTile
+                    nextPostId={(index+1)} 
+                    prevPostId={(index-1)}
+                    key={index}                                        
+                    postModalID={'postModal'+index}
+                    post={post} />    
+                );
+
+                this.setState({
+                    postTiles: userPosts
+                });
+        });
+    }
+
+    render(){
+        console.log('UserPostPage rendering ...');                
+        return(
+            <div className="app-content row center">
+                <h4 className="profilePageHeader">All Phones</h4>
+                {this.state.postTiles}
+                <p>this.props.params.type: {this.props.match.params.type}</p>                
+            </div>
+        )
+    }
+}
+
+export default PostTypePage;
