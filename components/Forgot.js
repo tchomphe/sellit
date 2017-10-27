eebe9807f9841767b8b4e5455c14cdb079e66174
admin <tashi.chomphel@gmail.com>
2017-10-27 02:31:13 -0400

    Minor modification on forgot and reset component.

--- components/Forgot.js
+++ components/Forgot.js
@@ -6,35 +6,41 @@ class Forgot extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            username: ""
+            email: "",
+            err: ""
         }
     }
     onSubmit(e){
         e.preventDefault();        
-        const username = this.refs.username.value;
+        const email = this.refs.email.value;
         Request
             .post('/forgot')
-            .send({email: username})
+            .send({email: email})
             .end(function(err, res){
                 if(err) {
                     alert('Error!');
                 } else {
-                    alert(`An email reset link has been sent to ${username}.`);                    
+                    alert(`An email reset link has been sent to ${email}.`);                    
                 }
             });
     }
     render(){        
-        return(
-            // <div className="col s6">
-                <div className="card-panel" style={{width: '50%'}}>
-                    <div className = "row">
-                        <form onSubmit={(e) => this.onSubmit(e)} >                    
-                            <label>Email: <input name="username" type="text" ref="username" placeholder="Enter your username"/></label>
-                            <button className="btn blue waves-effect waves-light" type="submit">Send confirmation</button>
-                        </form>                
+        return(            
+                <div className="col s12 m7 center">
+                    <div className="card large">
+                        <span className="card-title">Forgot your password?</span>
+                        <p className="left-align">We can send you a password reset link</p>
+                        <div className="card-content left-align">
+                            <form onSubmit={(e) => this.onSubmit(e)} >                    
+                                <input id="email" name="email" type="text" ref="email" placeholder="Enter your email" required="" aria-required="true" />
+                                <div className="card-action right-align">
+                                    <button className="btn blue waves-effect waves-light" type="submit">Send</button>
+                                    {/* <a href="#">This is a link</a> */}
+                                </div>                            
+                            </form>                
+                        </div>
                     </div>
-                </div>
-            // </div>
+                </div>        
         )
     }
 }
--- components/Reset.js
+++ components/Reset.js
@@ -8,10 +8,16 @@ class Reset extends React.Component{
         super(props);
         this.state = {
             password: "",
-            confirmPassword: ""
+            confirmPassword: "",
+            err: ""
         }
         this.handleInputChange = this.handleInputChange.bind(this);
     }
+
+    componentDidMount(){
+        $('.tooltipped').tooltip({delay: 50});        
+    }
+
     handleInputChange(event){
         const target = event.target;
         const name = target.name;
@@ -30,9 +36,10 @@ class Reset extends React.Component{
             .send({password: this.state.password})
             .end(function(err, res){
                 if(err) {
-                    alert('Error!');
+                    this.setState({err: res.body.error});
                 } else {
-                    alert('Your password has been reset.');                    
+                    Materialize.toast('Your password has been reset!', 4000);
+                    
                 }
             });            
         } else {
@@ -43,13 +50,19 @@ class Reset extends React.Component{
 
     render(){        
         return(
-            <div>
-                <h1>Reset</h1>                     
-                <form className="col s12" onSubmit={e => this.onSubmit(e)} >                    
-                    <label>New password: <input name="password" type="password" ref="password" value={this.state.password} onChange={this.handleInputChange} placeholder="Enter your new password"/></label>
-                    <label>Confirm new password: <input name="confirmPassword" type="password" ref="confirmPassword" value={this.state.confirmPassword} onChange={this.handleInputChange} placeholder="Confirm your new password"/></label>
-                    <button className = "btn-large waves-effect waves-light" type="submit"> Reset </button>
-                </form>                
+            <div className="col s12 m7 center">
+                <div className="card large">
+                    <span className="card-title">Reset your password</span>
+                    <div className="card-content left-align">
+                        <form className="col s12" onSubmit={e => this.onSubmit(e)} >                    
+                            <label>New password: <input name="password" type="password" ref="password" value={this.state.password} onChange={this.handleInputChange} placeholder="Enter your new password"/></label>
+                            <label>Confirm new password: <input name="confirmPassword" type="password" ref="confirmPassword" value={this.state.confirmPassword} onChange={this.handleInputChange} placeholder="Confirm your new password"/></label>
+                            <div className="card-action right-align">
+                                <button className = "btn blue waves-effect waves-light" type="submit"> Reset </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
         )
     }
