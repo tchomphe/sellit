71288ca3f89f38db422e39a480dbe099c9369e86
Zlatko Gantchev <zlatko.gantchev@gmail.com>
2017-10-28 13:53:59 -0400

    Included HTML body from PostEditModal

--- components/PostEditContainer.js
+++ components/PostEditContainer.js
@@ -50,11 +50,59 @@ class PostEditContainer extends React.Component{
 
     render(){
         console.log("PostEditContainer rendering..");
-        console.log('current state: ' + JSON.stringify(this.state, null, 4));
+
+        //map all post images into HTML elements
+        var postImagesHTML = "";
+        if (this.state.pictures)
+            postImagesHTML = this.state.pictures.map((pictureURL, index) => <img className="form-items-gallery-picture" src={pictureURL} alt="Post Picture" />);
 
         return(
             <div className="container">
                 <div className="red-text">{this.state.err}HELLO WORLD!</div>
+                <form id="postEditForm" onSubmit={this.handleSubmit}>
+                <div className="card-panel">
+                    <div className="row">
+                        <div className="form-items-info col s6">
+                            <h5>Post Info</h5>
+                            <InputField labelText="Title"
+                                id="title" value={this.state.title} required="required" onChange={this.handleInputChange} />
+                            <InputField fieldClass="col s6" labelText="Price"
+                                id="price" value={this.state.price} onChange={this.handleInputChange} />
+                            <div className="input-field col s6">
+                                <select id="type" name="type" required="required" >
+                                    <option value="Phone" defaultValue>Phone</option>
+                                    <option value="Laptop">Laptop</option>
+                                    <option value="Case">Case</option>
+                                    <option value="Other">Other</option>
+                                </select>
+                                <label>* Type</label>
+                            </div>
+                            <InputField labelText="Address"
+                                id="address" value={this.state.address} onChange={this.handleInputChange} />
+                            <InputField labelText="Description"
+                                id="description" value={this.state.description} onChange={this.handleInputChange} />
+                        </div>
+                        <div className="form-items-gallery col s6">
+                            <h5>Pictures</h5>
+                            <div className="col s12 file-field input-field">
+                                <div className="btn">
+                                    <span>Browse</span>
+                                    <input id="postImages" type="file" name="postImages" multiple/>
+                                </div>
+                                <div className="file-path-wrapper">
+                                    <input className="file-path validate" type="text" placeholder="Upload new pictures" />
+                                </div>
+                            </div>
+                            {postImagesHTML}
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="s12">
+                            <button className="btn waves-effect blue white-text darken-text-2" type="submit" value="Submit">Update</button>
+                        </div>
+                    </div>
+                </div>
+            </form>
             </div>
         );
     }
