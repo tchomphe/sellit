d8da6e006e6e440a155d5a97cce062e1a1df010d
admin <tashi.chomphel@gmail.com>
2017-10-30 16:54:10 -0400

    Added an extra div wrapper for PostTile.

--- components/PostTile.js
+++ components/PostTile.js
@@ -25,34 +25,37 @@ export default class PostTile extends React.Component {
         var thumbnailImage = (this.props.post.thumbnail) ? this.props.post.thumbnail : "https://placehold.it/350x250";
 
         return(
-            <div className="card">
-                <div className="card-image">
-                    <a href="#!" onClick={(e) => (this.handleSubmit(e))} className="waves-effect waves-light">
-                        <img src={thumbnailImage} alt="Card image cap" />
-                    </a>                    
+            <div>
+                <div className="card">
+                    <div className="card-image">
+                        <a href="#!" onClick={(e) => (this.handleSubmit(e))}>
+                            <img src={thumbnailImage} alt="Card image cap" />
+                        </a>                        
+                        <a className="btn-floating btn-large halfway-fab blue lighten-1">${this.props.post.price}</a>
+                    </div>
+
+                    <div className="card-content">                    
+                        <span className="card-title activator truncate">{this.props.post.title}<i className="material-icons right">keyboard_arrow_up</i></span>
+                        <p className="left-align card-text">{this.props.post.address}<i className="material-icons">location_on</i></p>
+                        <p className="left-align card-text">Posted on: {this.props.post.date}</p>
+                    </div>   
+
+                    <div className="card-reveal">
+                        <span className="card-title truncate">{this.props.post.title}<i className="material-icons right">close</i></span>
+                        <p className="left-align">{this.props.post.description}</p>
+                    </div>
+
+                    <PostModal
+                            prevPostId={this.props.prevPostId}
+                            nextPostId={this.props.nextPostId}                        
+                            modalID={this.props.postModalID}
+                            title={this.props.post.title}
+                            price={this.props.post.price}
+                            address={this.props.post.address}
+                            description={this.props.post.description}
+                            images={this.props.post.images}
+                            ownerId={this.props.post.ownerID} />
                 </div>
-
-                <div className="card-content">                    
-                    <span className="card-title activator truncate">{this.props.post.title}<i className="material-icons right">keyboard_arrow_up</i></span>
-                    <p className="left-align card-text">{this.props.post.address}<i className="material-icons">location_on</i></p>
-                    <p className="left-align card-text">${this.props.post.price}</p>
-                </div>   
-
-                <div className="card-reveal">
-                    <span className="card-title truncate">{this.props.post.title}<i className="material-icons right">close</i></span>
-                    <p className="left-align">{this.props.post.description}</p>
-                </div>
-
-                <PostModal
-                        prevPostId={this.props.prevPostId}
-                        nextPostId={this.props.nextPostId}                        
-                        modalID={this.props.postModalID}
-                        title={this.props.post.title}
-                        price={this.props.post.price}
-                        address={this.props.post.address}
-                        description={this.props.post.description}
-                        images={this.props.post.images}
-                        ownerId={this.props.post.ownerID} />
             </div>
         )
     }
