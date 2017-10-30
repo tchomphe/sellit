389a9e4c47fe9d8e44fad13ec83233e1dd7e52e9
admin <tashi.chomphel@gmail.com>
2017-10-30 16:52:24 -0400

    Modified confirm delete modal & added an extra div on the card.

--- components/InteractivePostTile.js
+++ components/InteractivePostTile.js
@@ -54,40 +54,43 @@ export default class InteractivePostTile extends React.Component {
         var thumbnailImage = (this.props.post.thumbnail) ? this.props.post.thumbnail : "https://placehold.it/350x250";
 
         return(
-            <div className="card">
-                {/* <a onClick={(e) => (this.handleEdit(e))} className="waves-effect waves-light btn-large blue"><i className="material-icons right">edit</i>EDIT</a>
-                <a onClick={(e) => (this.deleteConfirmation(e))} className="waves-effect waves-light btn-large red"><i className="material-icons right">delete</i>DELETE</a> */}
-                <div className="card-image">
-                    <a href="#!" onClick={(e) => (this.handlePreview(e))} className="waves-effect waves-light">
-                        <img src={thumbnailImage} alt="Card image cap" />
-                    </a>
+            <div>
+                <div className="card">                
+                    <div className="card-image">
+                        <a href="#!" onClick={(e) => (this.handlePreview(e))} className="waves-effect waves-light">
+                            <img src={thumbnailImage} alt="Card image cap" />
+                        </a>
+                    </div>
+                    <div className="card-content">
+                        <span className="card-title">{this.props.post.title}</span>
+                        <p className="card-text"><i className="tiny material-icons">location_on</i> {this.props.post.address}</p>
+                    </div>
+                    <div className="card-action right-align">
+                    <a href="#" style={{color: '#4285f4'}} onClick={(e) => (this.handleEdit(e))}>Edit</a>
+                    <a href="#" style={{color: 'red'}} onClick={(e) => (this.deleteConfirmation(e))}>Delete</a>
+                    </div>                
+                    <PostModal
+                        prevPostId={this.props.prevPostId}
+                        nextPostId={this.props.nextPostId}
+                        modalID={this.props.postModalID}
+                        title={this.props.post.title}
+                        price={this.props.post.price}
+                        address={this.props.post.address}
+                        description={this.props.post.description}
+                        images={this.props.post.images}
+                        ownerId={this.props.post.ownerID}
+                        email={'placeholder@placehoder.email'} />
                 </div>
-                <div className="card-content">
-                    <span className="card-title">{this.props.post.title}</span>
-                    <p className="card-text"><i className="tiny material-icons">location_on</i> {this.props.post.address}</p>
-                </div>
-                <div className="card-action right-align">
-                <a href="#" style={{color: '#4285f4'}} onClick={(e) => (this.handleEdit(e))}>Edit</a>
-                <a href="#" style={{color: 'red'}} onClick={(e) => (this.deleteConfirmation(e))}>Delete</a>
-                </div>
-                <div id={"delete_" + this.props.postModalID} className="modal">
-                    <div className="modal-content">
-                        <h5>Confirm Delete</h5>
-                        <a href="#!" className="modal-action modal-close waves-effect waves-red btn-flat ">Cancel</a>
-                        <a onClick={(e) => (this.handleDelete(e))} href="#!" className="modal-action modal-close waves-effect waves-green btn-flat ">Delete</a>
+                <div id={"delete_" + this.props.postModalID} className="modal" style={{'max-width':'400px'}}>
+                    <div className="modal-content left-align">
+                        <h5>Confirm</h5>
+                        <p>Are you sure you want to continue?</p>
+                    </div>
+                    <div className="modal-footer" style={{'background-color':'#fafafa'}}>
+                        <a href="#!" className="modal-action modal-close waves-effect waves-red btn-flat">Cancel</a>                        
+                        <a onClick={(e) => (this.handleDelete(e))} href="#!" className="modal-action modal-close waves-effect waves-red btn-flat">Delete</a>
                     </div>
                 </div>
-                <PostModal
-                    prevPostId={this.props.prevPostId}
-                    nextPostId={this.props.nextPostId}
-                    modalID={this.props.postModalID}
-                    title={this.props.post.title}
-                    price={this.props.post.price}
-                    address={this.props.post.address}
-                    description={this.props.post.description}
-                    images={this.props.post.images}
-                    ownerId={this.props.post.ownerID}
-                    email={'placeholder@placehoder.email'} />
             </div>
         )
     }
