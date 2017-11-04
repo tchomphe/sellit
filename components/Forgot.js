d0c38de19f6c0c4199b83a797d291bab013187ea
admin <tashi.chomphel@gmail.com>
2017-11-04 17:22:15 -0400

    Check if email input field is empty and display error if any.

--- components/Forgot.js
+++ components/Forgot.js
@@ -25,28 +25,32 @@ class Forgot extends React.Component{
 
     onSubmit(e){
         e.preventDefault();
-        Request
-            .post('/forgot')
-            .send({email: this.state.email})
-            .end(function(err, res){
-                if(err) {
-                    Materialize.toast('Error! Message could not be sent!', 4000);
-                } else {
-                    Materialize.toast('An email with reset link has been sent!', 4000);
-                }
-            });
+        if(this.state.email !== ""){
+            Request
+                .post('/forgot')
+                .send({email: this.state.email})
+                .end(function(err, res){
+                    if(err) {
+                        $('#forgot #email').removeClass("valid").addClass("invalid");
+                    } else {
+                        Materialize.toast('An email with reset link has been sent!', 4000);
+                    }
+                });
+        } else {
+            $('#forgot #email').removeClass("valid").addClass("invalid");
+        }
     }
     render(){
         return(
-                <div className="col s12 m7 center ">
+                <div id="forgot" className="col s12 m7 center">
                     <div className="card large">
                         <div className="container">
                             <div className="card-content left-align">
                             <form onSubmit={(e) => this.onSubmit(e)} >
                             <span className="card-title">Forgot your password?</span>
                             <p>We can send you a password reset link</p>
-                            <InputField labelText="Enter your email" labelSuccess="" labelError={(this.state.err)?this.state.err:"Enter a valid email address"}
-                                id="email" type="email" onChange={this.handleInputChange} value={this.state.email} required="" aria-required="true" />
+                            <InputField id="email" type="email" labelText="Enter your email" labelSuccess="" labelError={(this.state.err)?this.state.err:"Enter a valid email address"}
+                                onChange={this.handleInputChange} value={this.state.email} required="" aria-required="true" />
                                 {/* <input id="email" name="email" type="text" ref="email" placeholder="Enter your email" required="" aria-required="true" /> */}
                                 <div className="card-action right-align" style={{'padding-right':'20px'}}>
                                     <button className="btn black waves-effect waves-light" type="submit">Send</button>
