c78350495e5070418af9e01b9f88cdbf652058ad
Zlatko Gantchev <zlatko.gantchev@gmail.com>
2017-11-18 13:52:39 -0500

    Modified price styling for tiles

--- components/PostTile.js
+++ components/PostTile.js
@@ -31,7 +31,7 @@ export default class PostTile extends React.Component {
                         <a href="#!" onClick={(e) => (this.handleSubmit(e))}>
                             <img src={thumbnailImage} alt="Card image cap" />
                         </a>
-                        <div className="card-image-price">${this.props.post.price}</div>
+                        <div className="card-image-price">{(this.props.post.price) ? "$"+this.props.post.price : "--"}</div>
                     </div>
 
                     <div className="card-content">
