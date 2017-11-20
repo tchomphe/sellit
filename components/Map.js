c1e242ec98eb8ce3139a25a256b6cdf69027f811
admin <tashi.chomphel@gmail.com>
2017-11-20 18:11:14 -0500

    Integrated link on static map image.

--- components/Map.js
+++ components/Map.js
@@ -15,10 +15,11 @@ class Map extends Component {
     // Geocoding done in componentWillReceiveProps because props are empty in componentDidMount on it's first run.
     // First run is empty because MainPage calls PostModal with empty props in it's initial state.
     componentDidMount(){
-    var filtered_address = (this.props.postalCode).replace(/\s/g, "+");
+    // var filtered_address = (this.props.postalCode).replace(/\s/g, "+");
+    var filtered_address = this.props.postalCode;
     Request
       // .get("https://maps.googleapis.com/maps/api/geocode/json?address="+filtered_address.toString()+"&key=AIzaSyD86X7QJpRthF_OfvzcftUNlPg1kzg6sKo")
-      .get("https://maps.googleapis.com/maps/api/geocode/json?sensor=false&region=ca&components=postal_code:"+filtered_address.toString()+"&key=AIzaSyD86X7QJpRthF_OfvzcftUNlPg1kzg6sKo")
+      .get("https://maps.googleapis.com/maps/api/geocode/json?sensor=false&region=ca&components=postal_code:"+filtered_address+"&key=AIzaSyD86X7QJpRthF_OfvzcftUNlPg1kzg6sKo")
       .end((err, res) => {
         if(err){
           console.log('error!')
@@ -33,10 +34,11 @@ class Map extends Component {
     }
 
   render() {
+    var gotolink = "https://www.google.com/maps/place/"+this.state.lat+","+this.state.lng;
     var glink = "https://maps.googleapis.com/maps/api/staticmap?center="+this.state.lat+","+this.state.lng+"&zoom=14&size=500x120&maptype=roadmap&markers=color:red%7Clabel:C%7C"+this.state.lat+','+this.state.lng+"&key=AIzaSyBcxF_7FH1aEC4g6CGCvq7WPz1LCisZt3A";
     return (
       <div>
-        <img src={glink} style={{width: '100%'}}/>
+        <a target="_blank" href={gotolink}><img src={glink} style={{width: '100%'}}/></a>
       </div>
     );
   }
