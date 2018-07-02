e9370ed3687cc9269c4a6be210d6129128139f48
Zlatko Gantchev <zlatko.gantchev@gmail.com>
2018-07-01 21:03:47 -0400

    Converted double space tabbing to 4-spaces for consistency within all React Components

--- components/AppRoutes.js
+++ components/AppRoutes.js
@@ -22,104 +22,104 @@ import NavigationHeader from './NavigationHeader';
 import Request from 'superagent';
 
 export default class AppRoutes extends React.Component {
-  constructor(props){
-    super(props);
+    constructor(props){
+        super(props);
 
-    //define state variables
-    this.state = {
-      posts: [],
-      page: 1,
-      user: {
-        email: null,
-        nickname: null,
-        phone: null,
-        authorized: false,
-      }
-    };
+        //define state variables
+        this.state = {
+            posts: [],
+            page: 1,
+            user: {
+                email: null,
+                nickname: null,
+                phone: null,
+                authorized: false,
+            }
+        };
 
-    //bind functions to this component
-    this.saveUser = this.saveUser.bind(this);
-    this.forgetUser = this.forgetUser.bind(this);
-  }
+        //bind functions to this component
+        this.saveUser = this.saveUser.bind(this);
+        this.forgetUser = this.forgetUser.bind(this);
+    }
 
-  componentDidMount(){
-    //varify if the current user is authenticated
-    Request.get('/varifyAuthentication').then((res) => {
-      console.log('USER: ' + JSON.stringify(res.header.user));
-      if(res.header.user){
-        //parse user data into array, and then save as object in the state
-        let userArr = res.header.user.split(', ');
-        this.setState({
-          user: { email: userArr.shift(), nickname: userArr.shift(), phone: userArr.shift(), authorized: true }
+    componentDidMount(){
+        //varify if the current user is authenticated
+        Request.get('/varifyAuthentication').then((res) => {
+            console.log('USER: ' + JSON.stringify(res.header.user));
+            if(res.header.user){
+                //parse user data into array, and then save as object in the state
+                let userArr = res.header.user.split(', ');
+                this.setState({
+                    user: { email: userArr.shift(), nickname: userArr.shift(), phone: userArr.shift(), authorized: true }
+                });
+            }
         });
-      }
-    });
-  }
+    }
 
-  saveUser(authorizedUser){
-    this.setState({
-      user: {
-        email: authorizedUser.email,
-        nickname: (authorizedUser.nickname == 'null') ? '' : authorizedUser.nickname,
-        phone: (authorizedUser.phone == 'null') ? '' : authorizedUser.phone,
-        authorized: true,
-      }
-    });
-  }
+    saveUser(authorizedUser){
+        this.setState({
+            user: {
+                email: authorizedUser.email,
+                nickname: (authorizedUser.nickname == 'null') ? '' : authorizedUser.nickname,
+                phone: (authorizedUser.phone == 'null') ? '' : authorizedUser.phone,
+                authorized: true,
+            }
+        });
+    }
 
-  forgetUser(event){
-    event.preventDefault();
-    //show page loading wrapper
-    $("#dim-page-loader").fadeIn(100);
+    forgetUser(event){
+        event.preventDefault();
+        //show page loading wrapper
+        $("#dim-page-loader").fadeIn(100);
 
-    //send logout request
-    Request
-      .get('/logout')
-      .end((err, res) => {
-        //hide page loading wrapper
-        $("#dim-page-loader").fadeOut(100);
+        //send logout request
+        Request
+            .get('/logout')
+            .end((err, res) => {
+                //hide page loading wrapper
+                $("#dim-page-loader").fadeOut(100);
 
-        if(err){
-          Materialize.toast('Error logging out. Please refresh the page.', 4000);
-          console.log("Logout Error log -> " + res.body.error);
-        }
-        else {
-          Materialize.toast('Logged out.', 4000);
-          //update the state
-          this.setState({ user: { email: null, nickname: null, phone: null, authorized: false } });
-        }
-      });
-  }
+                if(err){
+                    Materialize.toast('Error logging out. Please refresh the page.', 4000);
+                    console.log("Logout Error log -> " + res.body.error);
+                }
+                else {
+                    Materialize.toast('Logged out.', 4000);
+                    //update the state
+                    this.setState({ user: { email: null, nickname: null, phone: null, authorized: false } });
+                }
+            });
+    }
 
-  render() {
-    console.log('AppRoutes rendering... ');
+    render() {
+        console.log('AppRoutes rendering... ');
 
-    return (
-      <Router history={hashHistory}>
-        <Layout>
-          <NavigationHeader authorizedUser={this.state.user.authorized} handleLogout={this.forgetUser} />
+        return (
+            <Router history={hashHistory}>
+                <Layout>
+                    <NavigationHeader authorizedUser={this.state.user.authorized} handleLogout={this.forgetUser} />
 
-          <Switch>
-            <Route path="/" exact={true} render={() => (
-              <MainPage posts={this.state.posts} page={this.state.page} />)} />
-            <Route path="/search/:query" component={SearchResultsPage} />
-            <Route path="/create-post" component={PostCreationContainer} />
-            <Route path="/edit-post/:postId" component={PostEditContainer} />
-            <Route path="/my-posts" component={MyPostsPage} />
-            <Route path="/user-posts/:ownerId" component={UserPostPage} />
-            <Route path="/post/:id" component={SinglePostPage} />
-            <Route path="/forgot" component={Forgot} />
-            <Route path="/reset/:token" component={Reset} />
-            <Route path="/posts/:type" component={PostTypePage} />
-            <Route path="/my-account" render={() => (
-              <MyAccountPage user={this.state.user} />)} />
-            <Route component={NotFoundPage} />
-          </Switch>
+                    <Switch>
+                        <Route path="/" exact={true} render={() => (
+                            <MainPage posts={this.state.posts} page={this.state.page} />)} />
+                        <Route path="/search/:query" component={SearchResultsPage} />
+                        <Route path="/create-post" component={PostCreationContainer} />
+                        <Route path="/edit-post/:postId" component={PostEditContainer} />
+                        <Route path="/my-posts" component={MyPostsPage} />
+                        <Route path="/user-posts/:ownerId" component={UserPostPage} />
+                        <Route path="/post/:id" component={SinglePostPage} />
+                        <Route path="/forgot" component={Forgot} />
+                        <Route path="/reset/:token" component={Reset} />
+                        <Route path="/posts/:type" component={PostTypePage} />
+                        <Route path="/my-account" render={() => (
+                            <MyAccountPage user={this.state.user} />)} />
+                        <Route component={NotFoundPage} />
+                    </Switch>
 
-          <UserRegistrationModal handleLogin={this.saveUser} />
-          <UserLoginModal handleLogin={this.saveUser} />
-        </Layout>
-      </Router>
-    );
-  }
+                    <UserRegistrationModal handleLogin={this.saveUser} />
+                    <UserLoginModal handleLogin={this.saveUser} />
+                </Layout>
+            </Router>
+        );
+    }
 }
--- components/Map.js
+++ components/Map.js
@@ -2,46 +2,47 @@ import React, { Component } from 'react';
 import Request from 'superagent';
 
 class Map extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      postalCode: this.props.postalCode,
-      lat: "",
-      lng: "",
-      err: "",
+    constructor(props){
+        super(props);
+        this.state = {
+            postalCode: this.props.postalCode,
+            lat: "",
+            lng: "",
+            err: "",
+        }
     }
-  }
 
-  // Geocoding done in componentWillReceiveProps because props are empty in componentDidMount on it's first run.
-  // First run is empty because MainPage calls PostModal with empty props in it's initial state.
-  componentDidMount(){
-  // var filtered_address = (this.props.postalCode).replace(/\s/g, "+");
-  var filtered_address = this.props.postalCode;
-  Request
-    // .get("https://maps.googleapis.com/maps/api/geocode/json?address="+filtered_address.toString()+"&key=AIzaSyD86X7QJpRthF_OfvzcftUNlPg1kzg6sKo")
-    .get("https://maps.googleapis.com/maps/api/geocode/json?sensor=false&region=ca&components=postal_code:"+filtered_address+"&key=AIzaSyD86X7QJpRthF_OfvzcftUNlPg1kzg6sKo")
-    .end((err, res) => {
-      if(err){
-        console.log('error!')
-      }
-      else {
-        this.setState({
-          lat: res.body.results[0].geometry.location.lat,
-          lng: res.body.results[0].geometry.location.lng,
-        });
-      }
-    });
-  }
+    // Geocoding done in componentWillReceiveProps because props are empty in componentDidMount on it's first run.
+    // First run is empty because MainPage calls PostModal with empty props in it's initial state.
+    componentDidMount(){
+        // var filtered_address = (this.props.postalCode).replace(/\s/g, "+");
+        var filtered_address = this.props.postalCode;
 
-  render() {
-    var gotolink = "https://www.google.com/maps/place/"+this.state.lat+","+this.state.lng;
-    var glink = "https://maps.googleapis.com/maps/api/staticmap?center="+this.state.lat+","+this.state.lng+"&zoom=14&size=500x120&maptype=roadmap&markers=color:red%7Clabel:C%7C"+this.state.lat+','+this.state.lng+"&key=AIzaSyBcxF_7FH1aEC4g6CGCvq7WPz1LCisZt3A";
-    return (
-      <div>
-        <a target="_blank" href={gotolink}><img src={glink} style={{width: '100%'}}/></a>
-      </div>
-    );
-  }
+        Request
+            // .get("https://maps.googleapis.com/maps/api/geocode/json?address="+filtered_address.toString()+"&key=AIzaSyD86X7QJpRthF_OfvzcftUNlPg1kzg6sKo")
+            .get("https://maps.googleapis.com/maps/api/geocode/json?sensor=false&region=ca&components=postal_code:"+filtered_address+"&key=AIzaSyD86X7QJpRthF_OfvzcftUNlPg1kzg6sKo")
+            .end((err, res) => {
+                if(err){
+                    console.log('error!')
+                }
+                else {
+                    this.setState({
+                        lat: res.body.results[0].geometry.location.lat,
+                        lng: res.body.results[0].geometry.location.lng,
+                    });
+                }
+            });
+    }
+
+    render() {
+        var gotolink = "https://www.google.com/maps/place/"+this.state.lat+","+this.state.lng;
+        var glink = "https://maps.googleapis.com/maps/api/staticmap?center="+this.state.lat+","+this.state.lng+"&zoom=14&size=500x120&maptype=roadmap&markers=color:red%7Clabel:C%7C"+this.state.lat+','+this.state.lng+"&key=AIzaSyBcxF_7FH1aEC4g6CGCvq7WPz1LCisZt3A";
+        return (
+            <div>
+                <a target="_blank" href={gotolink}><img src={glink} style={{width: '100%'}}/></a>
+            </div>
+        );
+    }
 }
 
 export default Map;
